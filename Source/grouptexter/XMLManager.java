7591498447ac564c90fcb7df5516d691f728d8c3
Ethan Schulz <eson.ignis@gmail.com>
2013-10-16 23:35:29 -0500

    Started on XMLManager class.

---
+++ Source/grouptexter/XMLManager.java
@@ -0,0 +1,14 @@
+package grouptexter;
+
+import java.util.ArrayList;
+
+/**
+ *
+ * @author Eson
+ */
+public class XMLManager {
+    
+    public static ArrayList<Person> readPeople(){
+       
+    }
+}
