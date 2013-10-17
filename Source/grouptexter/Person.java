66fe949fff7cd20736a121af0f9423be12248003
Ethan Schulz <eson.ignis@gmail.com>
2013-10-16 23:25:25 -0500

    Created person class.

---
+++ Source/grouptexter/Person.java
@@ -0,0 +1,45 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package grouptexter;
+
+/**
+ *
+ * @author Eson
+ */
+public class Person {
+    private String firstName;
+    private String lastName;
+    private String number;
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getNumber() {
+        return number;
+    }
+
+    public void setNumber(String number) {
+        this.number = number;
+    }
+
+    public Person(String firstName, String lastName, String number) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.number = number;
+    }
+}
